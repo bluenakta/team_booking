31d3cbfd8a72fcda17360680531853c1738b1343
bluenakta <mapoajj1@gmail.com>
2020-06-15 16:43:28 +0900

    event 분리

--- src/main/java/show/Booking.java
+++ src/main/java/show/Booking.java
@@ -1,65 +1,75 @@
 package show;
 
-import javax.persistence.*;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.PostPersist;
+import javax.persistence.PrePersist;
+import javax.persistence.PreRemove;
+import javax.persistence.Table;
+
 import org.springframework.beans.BeanUtils;
-import java.util.List;
 
 @Entity
-@Table(name="Booking_table")
+@Table(name = "Booking_table")
 public class Booking {
 
-    @Id
-    @GeneratedValue(strategy=GenerationType.AUTO)
-    private Long id;
-    private String showId;
-    private Integer qty;
-    private Integer amount;
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long id;
+	private String showId;
+	private Integer qty;
+	private Integer amount;
 
-    @PreRemove
-    public void onPreRemove(){
-        Booked booked = new Booked();
-        BeanUtils.copyProperties(this, booked);
-        booked.publishAfterCommit();
+	@PreRemove
+	public void onPreRemove() {
 
+		BookingCanceled bookingCanceled = new BookingCanceled();
+		BeanUtils.copyProperties(this, bookingCanceled);
+		bookingCanceled.publishAfterCommit();
 
-        BookingCanceled bookingCanceled = new BookingCanceled();
-        BeanUtils.copyProperties(this, bookingCanceled);
-        bookingCanceled.publishAfterCommit();
+	}
 
+	@PostPersist
+	public void onPostPersist() {
+		
+		Booked booked = new Booked();
+		BeanUtils.copyProperties(this, booked);
+		booked.publishAfterCommit();
 
-    }
+	}
 
+	public Long getId() {
+		return id;
+	}
 
-    public Long getId() {
-        return id;
-    }
+	public void setId(Long id) {
+		this.id = id;
+	}
 
-    public void setId(Long id) {
-        this.id = id;
-    }
-    public String getShowId() {
-        return showId;
-    }
+	public String getShowId() {
+		return showId;
+	}
 
-    public void setShowId(String showId) {
-        this.showId = showId;
-    }
-    public Integer getQty() {
-        return qty;
-    }
+	public void setShowId(String showId) {
+		this.showId = showId;
+	}
 
-    public void setQty(Integer qty) {
-        this.qty = qty;
-    }
-    public Integer getAmount() {
-        return amount;
-    }
+	public Integer getQty() {
+		return qty;
+	}
 
-    public void setAmount(Integer amount) {
-        this.amount = amount;
-    }
+	public void setQty(Integer qty) {
+		this.qty = qty;
+	}
 
+	public Integer getAmount() {
+		return amount;
+	}
 
-
+	public void setAmount(Integer amount) {
+		this.amount = amount;
+	}
 
 }
